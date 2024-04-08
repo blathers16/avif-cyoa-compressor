aba7ddc7c219ed062d814892b3d0d39bb2686501
David Calawa <blathersbugs@gmail.com>
2024-04-08 15:50:04 -0700

    version 2.0

--- src/app/compressor/compressor.component.ts
+++ src/app/compressor/compressor.component.ts
@@ -13,7 +13,8 @@ import {
   concatAll,
   from,
   map,
-  reduce
+  reduce,
+  tap
 } from 'rxjs';
 
 interface DownloadData {
@@ -48,25 +49,19 @@ export class CompressorComponent {
   progressText: string = '';
 
   MIME = RegExp('image/([a-z]+)');
+  // regex for finding data image dataURLs
+  // currently setup to find jpeg, jpg, png, webp, and gif
   DATAURL = RegExp(
-    '(["\'`]data:image/(?:j?pe?n?g|webp);base64,[a-zA-Z0-9+/]+={0,2}["\'`])'
+    '(["\'`]data:image/(?:j?pe?n?g|webp|gif);base64,[a-zA-Z0-9+/]+={0,2}["\'`])'
   ); // parens for split()
-  isDataURL = (s: string) =>
-    s.match(
-      '^(["\'`]data:image/(?:j?pe?n?g|webp);base64,[a-zA-Z0-9+/]+={0,2}["\'`])$'
-    ) && s[0] == s.slice(-1);
-  fileSizesSum = (files: File[]) =>
-    Array.from(files)
-      .map((x) => x.size)
-      .reduce((n, m) => n + m, 0);
+  // formatter for file sizes
   _num = (n: string) => {
     const match = `${n}`.match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g);
-    return (match ? match : ['']).join(' ');
+    return `${(match ? match : ['']).join(',')} Bytes`;
   };
-  $node = (tag: any, options = {}) =>
-    Object.assign(document.createElement(tag), options);
-  params = () => ['image/webp', `${this.quality}/100`];
 
+  // dispatcher for web workers using observable-webworker
+  // https://github.com/cloudnc/observable-webworker
   convertText(s: any[]): Observable<orderedString[]> {
     return fromWorkerPool<orderedString, orderedString[]>(
       () =>
@@ -77,13 +72,23 @@ export class CompressorComponent {
     );
   }
 
-  counter: number = 0;
 
+  // main function to setup conversion
+  // takes a File object as input
+  // todo: figure out typing for return type
   convert(file: File): any {
+    // turn our file into a string
     return from(file.text()).pipe(
-      map((x) => x.split(this.DATAURL)),
+      // split the string into an array of strings
+      // that are the dataURLs and the stuff before and after them
+      map((x: string): string[] => x.split(this.DATAURL)),
+      // annotate our strings with their index in the array so we
+      // can put them back together in the right order later
       map((x) => x.map((st: string , i: number): orderedString => {return { s: st, index: i}})),
-      map((x) => this.convertText(x)),
+      // send the strings off to the dispatch function
+      // data urls will be converted, and others will be returned
+      // as-is
+      map((x: orderedString[]): Observable<orderedString[]> => this.convertText(x)),
       concatAll()
     );
   }
@@ -103,8 +108,12 @@ sortResults(a: orderedString, b: orderedString): number{
   async process(e: Event): Promise<void> {
     const target = e.target as HTMLInputElement;
     let infiles = target.files;
+    // incase you canceled the file select, you won't lose your
+    // previous result
     if (!infiles) return;
     this.results.forEach((result: DownloadData) =>
+      // revoke any references to previously compressed
+      // CYOAs to free memory
       URL.revokeObjectURL(result.href)
     );
     this.results = [];
@@ -113,15 +122,21 @@ sortResults(a: orderedString, b: orderedString): number{
 
     await this.convert(infile)
       .pipe(
-        reduce((acc: orderedString[], curr: orderedString) => [...acc, curr], [] as orderedString[])
+        // collect all the results together into one array
+        reduce((acc: orderedString[], value: orderedString): orderedString[] => [...acc, value], [] as orderedString[])
       )
-      .subscribe((convertedFiles: any) => {
+      .subscribe((convertedFiles: orderedString[]) => {
+        // these arrive in whatever order they convert in, so we need to sort them
         convertedFiles.sort(this.sortResults)
+        // remove the annotation used for sorting
         const withoutIndices: string[] = convertedFiles.map((x: orderedString) => x.s)
+        // join to a single string
         const fileString: string = withoutIndices.join('')
+        // convert to a blob
         const blob = new Blob([fileString], { type: infile.type });
-
+        // and create a file
         const outfile = new File([blob], infile.name, { type: infile.type });
+        // and push to DOM
         this.results.push({
           href: URL.createObjectURL(outfile),
           download: outfile.name,
--- src/app/compressor/compressor.worker.ts
+++ src/app/compressor/compressor.worker.ts
@@ -1,5 +1,6 @@
 /// <reference lib="webworker" />
 import encode, { init as initAvifEncode } from '@jsquash/avif/encode';
+import { ImageMagick, MagickFormat, initializeImageMagick } from '../../../node_modules/@imagemagick/magick-wasm';
 
 import { DoWorkUnit, runWorker } from 'observable-webworker';
 import { Observable, from } from 'rxjs';
@@ -7,7 +8,7 @@ import { encode as b64encode } from 'base64-arraybuffer';
 
 initAvifEncode(undefined, {
   // Customise the path to load the wasm file
-  locateFile: (path: string, prefix: string) => `./assets/avif_enc.wasm`,
+  locateFile: (path: string, prefix: string) => './assets/avif_enc.wasm'
 });
 
 interface orderedString {
@@ -19,14 +20,45 @@ export class CompressorWorker implements DoWorkUnit<orderedString, orderedString
   public workUnit(input: orderedString): Observable<orderedString> {
     return from(this.convert(input));
   }
+  MIME = RegExp('image/([a-z]+)');
   isDataURL = (s: string) =>
     s.match(
-      '^(["\'`]data:image/(?:j?pe?n?g|webp);base64,[a-zA-Z0-9+/]+={0,2}["\'`])$'
+      '^(["\'`]data:image/(?:j?pe?n?g|webp|gif);base64,[a-zA-Z0-9+/]+={0,2}["\'`])$'
     ) && s[0] == s.slice(-1);
   async convert(st: orderedString): Promise<orderedString> {
    const { s, index } = st;
+    // if this string is an image
     if (this.isDataURL(s)) {
+      // fetch it as a blob
       const blob = await (await fetch(s.slice(1, -1))).blob();
+
+      // detect animated gifs; convert to animated webp
+      // todo: update to use animated avif when
+      // https://github.com/ImageMagick/ImageMagick/issues/6380
+      // is fixed
+      if ((this.isGif(s) && await this.isGifAnimated(s)) ) {
+        const byteArray = new Uint8Array(await blob.arrayBuffer());
+        // there probably aren't a lot of animated gifs
+        // so lets only initialize imagemagik if needed
+        await initializeImageMagick(new URL(`${location.origin}/assets/magick.wasm`, import.meta.url))
+        // use readCollection to read in all frames of the gif
+        const imageBytes = ImageMagick.readCollection(byteArray,  (image) => {
+          const result = image.write(MagickFormat.Webp, data => {
+            // we have to copy the image to a new object because
+            // the memory will be freed by the
+            // web assembly code when it leaves this
+            // scope
+            const data2 = this.copyUint8Array(data)
+            return data2
+          }
+          )
+          return result;
+        })
+        const base64 = this.Uint8ToBase64(imageBytes);
+        return {s: `"data:image/webp;base64,${base64}"`, index: index}
+      } 
+
+      // convert to imageData for avif encoder
       const image = await createImageBitmap(blob);
       const canvas = new OffscreenCanvas(image.width, image.height);
       const ctx = canvas.getContext('2d');
@@ -35,10 +67,86 @@ export class CompressorWorker implements DoWorkUnit<orderedString, orderedString
 
       const avifBuffer = await encode(imageData!);
       const base64 = b64encode(avifBuffer);
-      return {s: `"data:image/avif;base64,${base64}"`, index: index}  ;
+      return {s: `"data:image/avif;base64,${base64}"`, index: index};
     } else {
+      // if it isn't a image we return the string
+      // as is
       return {s: s, index: index};
     }
+  }
+
+  isGif(s: string): boolean {
+    return s.slice(12, 15) === "gif"
+  }
+
+  isWebP(s: string): boolean {
+    return s.slice(12, 16) === "webp"
+  }
+
+  // somebody posted this in a github issue
+
+  copyUint8Array(sourceArray: Uint8Array) {
+    // Create a new Uint8Array with the same length as the source array
+    const copiedArray = new Uint8Array(sourceArray.length);
+    
+        // Iterate through the source array and copy its elements to the new array
+        for (let i = 0; i < sourceArray.length; i++) {
+            copiedArray[i] = sourceArray[i];
+        }
+        // Return the new Uint8Array
+        return copiedArray;
+    }
+
+  // code below is courtesy of stack overflow
+
+  Uint8ToBase64(u8Arr: Uint8Array): string{
+    var CHUNK_SIZE = 0x8000; //arbitrary number
+    var index = 0;
+    var length = u8Arr.length;
+    var result = '';
+    var slice;
+    while (index < length) {
+      slice = u8Arr.subarray(index, Math.min(index + CHUNK_SIZE, length)); 
+      result += String.fromCharCode.apply(null, slice as unknown as number[]);
+      index += CHUNK_SIZE;
+    }
+    return btoa(result);
+  }
+
+  HEADER_LEN: number = 6; // offset bytes for the header section
+  LOGICAL_SCREEN_DESC_LEN: number = 7; // offset bytes for logical screen description section
+
+  async isGifAnimated(s:string): Promise<Boolean> {
+    const blob = await (await fetch(s.slice(1, -1))).blob();
+    const buffer = await blob.arrayBuffer();
+    // Start from last 4 bytes of the Logical Screen Descriptor
+    const dv = new DataView(buffer, this.HEADER_LEN + this.LOGICAL_SCREEN_DESC_LEN - 3);
+    const globalColorTable = dv.getUint8(0); // aka packet byte
+    let globalColorTableSize = 0;
+    let offset = 0;
+
+    // check first bit, if 0, then we don't have a Global Color Table
+    if (globalColorTable & 0x80) {
+      // grab the last 3 bits, to calculate the global color table size -> RGB * 2^(N+1)
+      // N is the value in the last 3 bits.
+      globalColorTableSize = 3 * Math.pow(2, (globalColorTable & 0x7) + 1);
+    }
+
+    // move on to the Graphics Control Extension
+    offset = 3 + globalColorTableSize;
+
+    var extensionIntroducer = dv.getUint8(offset);
+    var graphicsConrolLabel = dv.getUint8(offset + 1);
+    var delayTime = 0;
+
+    // Graphics Control Extension section is where GIF animation data is stored
+    // First 2 bytes must be 0x21 and 0xF9
+    if (extensionIntroducer & 0x21 && graphicsConrolLabel & 0xf9) {
+      // skip to the 2 bytes with the delay time
+      delayTime = dv.getUint16(offset + 4);
+    }
+
+    return Boolean(delayTime);
   }
 }
 
