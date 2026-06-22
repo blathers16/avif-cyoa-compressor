08390443c3ab4f72828ccef174d4cdc533f0bb57
David Calawa <blathersbugs@gmail.com>
2026-06-22 16:42:14 -0700

    update angular version

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from "@angular/core";
 /// <reference types="@angular/localize" />
 import '@angular/localize/init';
 
@@ -5,5 +6,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]})
   .catch((err) => console.error(err));
